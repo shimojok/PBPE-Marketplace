31d3eb57c166c7605c31d638f2e3a9f0af88466a
Kaz Shimojo <shimojok@terraviss.com>
2026-05-24 14:47:02 +0900

    Add GHGStackedBar component for chart display

---
+++ frontend/src/components/charts/GHGStackedBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const GHGStackedBar = ({ data }) => {
+  return (
+    <div className="chart-container">
+      {/* Chart.js or Recharts implementation placeholder */}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
