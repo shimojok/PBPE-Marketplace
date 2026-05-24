a561f34464be1ecd2b26a38b25553b545874b378
Kaz Shimojo <shimojok@terraviss.com>
2026-05-24 16:30:51 +0900

    Update FinanceView.tsx

--- frontend/src/pages/Dashboard/FinanceView.tsx
+++ frontend/src/pages/Dashboard/FinanceView.tsx
@@ -3,7 +3,7 @@ import { getBondDashboard } from "../../api/dashboard";
 import { BondsTable } from "../../components/tables/BondsTable";
 
 export default function FinanceView() {
-  const [bonds, setBonds] = useState(null);
+  const [bonds, setBonds] = useState<any | null>(null);
 
   useEffect(() => {
     getBondDashboard().then(setBonds);
