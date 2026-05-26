7fb45e07da1f223c4942b0b4a3ebc9e76c14badb
Kaz Shimojo <shimojok@terraviss.com>
2026-05-26 23:03:02 +0900

    Update GHGStackedBar.tsx

--- frontend/src/components/charts/GHGStackedBar.tsx
+++ frontend/src/components/charts/GHGStackedBar.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import {
+  BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer
+} from "recharts";
 
 export const GHGStackedBar = ({ data }) => {
+  const chartData = data.sources.map(s => ({
+    name: s.name,
+    value: s.tco2e_per_year,
+  }));
+
   return (
-    <div className="chart-container">
-      {/* Chart.js or Recharts implementation placeholder */}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={chartData}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value" stackId="a" fill="#4CAF50" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
