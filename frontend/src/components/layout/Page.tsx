1fb7961919f9c3698a440e0180aec5de4ed56401
Kaz Shimojo <shimojok@terraviss.com>
2026-06-14 22:27:32 +0900

    Delete frontend/src/components/layout directory

--- frontend/src/components/layout/Page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page({ title, children }) {
-  return (
-    <div className="dashboard-page">
-      <h1 className="page-title">{title}</h1>
-      {children}
-    </div>
-  );
-}
